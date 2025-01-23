3cfaf9d11e2554e0c72369f9f4a998717d9783e0
Zess <zess@inven.co.kr>
2025-01-23 12:21:31 +0900

    history 스타일 수정 (이미지 24px -> 32px)

--- src/components/parts/main/history.tsx
+++ src/components/parts/main/history.tsx
@@ -9,10 +9,10 @@ export default function History() {
 
       <div>
         <div className="group relative flex gap-x-5">
-          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
-            <div className="relative z-10 size-6 flex justify-center items-center">
+          <div className="relative group-last:after:hidden after:absolute after:top-10 after:bottom-2 after:start-4 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
+            <div className="relative z-10 size-8 flex justify-center items-center">
               <Image
-                className="shrink-0 size-6 rounded-full"
+                className="shrink-0 size-8 rounded-full"
                 src="/inven2.png"
                 width="32"
                 height="32"
@@ -27,12 +27,12 @@ export default function History() {
             </h3>
 
             <p className="font-semibold text-sm text-gray-800 dark:text-neutral-200">
-              Web Designer &amp; Web Developer
+              풀스택 웹 개발 (선임 개발자)
             </p>
 
             <p className="mt-1 text-sm text-gray-600 dark:text-neutral-400">
-              The company has high expectations and using OKRs there is a mutual
-              understanding of expectations and performance.
+              인벤 KR의 각종 신규 프로젝트를 진행 및 시스템 유지보수를
+              진행하였습니다.
             </p>
 
             <ul className="list-disc ms-6 mt-3 space-y-1.5">
@@ -58,14 +58,14 @@ export default function History() {
         </div>
 
         <div className="group relative flex gap-x-5">
-          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
-            <div className="relative z-10 size-6 flex justify-center items-center">
+          <div className="relative group-last:after:hidden after:absolute after:top-10 after:bottom-2 after:start-4 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
+            <div className="relative z-10 size-8 flex justify-center items-center">
               <Image
-                className="shrink-0 size-6 rounded-full"
-                src="/suwon.jpg"
+                className="shrink-0 size-8 rounded-full"
+                src="/suwon2.png"
                 width="32"
                 height="32"
-                alt="Inven Logo"
+                alt="수원대학교 로고"
               />
             </div>
           </div>
@@ -113,69 +113,13 @@ export default function History() {
         </div>
 
         <div className="group relative flex gap-x-5">
-          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
-            <div className="relative z-10 size-6 flex justify-center items-center">
+          <div className="relative group-last:after:hidden after:absolute after:top-10 after:bottom-2 after:start-4 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
+            <div className="relative z-10 size-8 flex justify-center items-center">
               <svg
-                className="shrink-0 size-6 text-gray-600 dark:text-neutral-400"
+                className="shrink-0 size-8 text-gray-800 dark:text-neutral-200"
+                xmlns="http://www.w3.org/2000/svg"
                 width="32"
                 height="32"
-                viewBox="0 0 32 32"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M11.7326 0C9.96372 0.00130479 8.53211 1.43397 8.53342 3.19935C8.53211 4.96473 9.96503 6.39739 11.7339 6.39869H14.9345V3.20065C14.9358 1.43527 13.5029 0.00260958 11.7326 0C11.7339 0 11.7339 0 11.7326 0M11.7326 8.53333H3.20053C1.43161 8.53464 -0.00130383 9.9673 3.57297e-06 11.7327C-0.00261123 13.4981 1.4303 14.9307 3.19922 14.9333H11.7326C13.5016 14.932 14.9345 13.4994 14.9332 11.734C14.9345 9.9673 13.5016 8.53464 11.7326 8.53333V8.53333Z"
-                  fill="#36C5F0"
-                ></path>
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M32 11.7327C32.0013 9.9673 30.5684 8.53464 28.7995 8.53333C27.0306 8.53464 25.5976 9.9673 25.5989 11.7327V14.9333H28.7995C30.5684 14.932 32.0013 13.4994 32 11.7327ZM23.4666 11.7327V3.19935C23.4679 1.43527 22.0363 0.00260958 20.2674 0C18.4984 0.00130479 17.0655 1.43397 17.0668 3.19935V11.7327C17.0642 13.4981 18.4971 14.9307 20.2661 14.9333C22.035 14.932 23.4679 13.4994 23.4666 11.7327Z"
-                  fill="#2EB67D"
-                ></path>
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M20.2661 32C22.035 31.9987 23.4679 30.566 23.4666 28.8007C23.4679 27.0353 22.035 25.6026 20.2661 25.6013H17.0656V28.8007C17.0642 30.5647 18.4972 31.9974 20.2661 32ZM20.2661 23.4654H28.7995C30.5684 23.4641 32.0013 22.0314 32 20.266C32.0026 18.5006 30.5697 17.068 28.8008 17.0654H20.2674C18.4985 17.0667 17.0656 18.4993 17.0669 20.2647C17.0656 22.0314 18.4972 23.4641 20.2661 23.4654V23.4654Z"
-                  fill="#ECB22E"
-                ></path>
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M8.93953e-07 20.266C-0.00130651 22.0314 1.43161 23.4641 3.20052 23.4654C4.96944 23.4641 6.40235 22.0314 6.40105 20.266V17.0667H3.20052C1.43161 17.068 -0.00130651 18.5006 8.93953e-07 20.266ZM8.53342 20.266V28.7993C8.53081 30.5647 9.96372 31.9974 11.7326 32C13.5016 31.9987 14.9345 30.566 14.9332 28.8007V20.2686C14.9358 18.5032 13.5029 17.0706 11.7339 17.068C9.96372 17.068 8.53211 18.5006 8.53342 20.266C8.53342 20.2673 8.53342 20.266 8.53342 20.266Z"
-                  fill="#E01E5A"
-                ></path>
-              </svg>
-            </div>
-          </div>
-
-          <div className="grow pb-8 group-last:pb-0">
-            <h3 className="mb-1 text-xs text-gray-600 dark:text-neutral-400">
-              2011 - 2021
-            </h3>
-
-            <p className="font-semibold text-sm text-gray-800 dark:text-neutral-200">
-              Junior Software Engineer at Slack
-            </p>
-
-            <p className="mt-1 text-sm text-gray-600 dark:text-neutral-400">
-              Work in Slack is one of the beautiful experience I can do in my
-              entire life. There are a lot of interesting thing to learn and
-              manager respect your time and your personality.
-            </p>
-          </div>
-        </div>
-
-        <div className="group relative flex gap-x-5">
-          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
-            <div className="relative z-10 size-6 flex justify-center items-center">
-              <svg
-                className="shrink-0 size-6 text-gray-800 dark:text-neutral-200"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
@@ -193,28 +137,12 @@ export default function History() {
 
           <div className="grow pb-8 group-last:pb-0">
             <h3 className="mb-1 text-xs text-gray-600 dark:text-neutral-400">
-              2010 - 2011
+              2006 - 2009
             </h3>
 
             <p className="font-semibold text-sm text-gray-800 dark:text-neutral-200">
-              Freelance Graphic Designer
+              광영남자고등학교 졸업
             </p>
-
-            <ul className="list-disc ms-6 mt-3 space-y-1.5">
-              <li className="ps-1 text-sm text-gray-600 dark:text-neutral-400">
-                Worked with a diverse range of clients, delivering tailored
-                design solutions.
-              </li>
-              <li className="ps-1 text-sm text-gray-600 dark:text-neutral-400">
-                Developed and maintained strong client relationships through
-                effective communication and project management.
-              </li>
-              <li className="ps-1 text-sm text-gray-600 dark:text-neutral-400">
-                Utilized tools such as Notion for project tracking, Mailchimp
-                for email marketing designs, Slack for team collaboration, and
-                GitHub for version control and project sharing.
-              </li>
-            </ul>
           </div>
         </div>
       </div>
