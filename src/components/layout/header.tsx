ff2d807e47d12c3c61f74a3a303b1271b539e024
grooveTurtle <grooveturtle29@gmail.com>
2025-07-18 02:40:22 +0900

    네비게이션 스타일 수정

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -10,7 +10,7 @@ const sections = [
   { id: "main-history", title: "History" },
   { id: "main-license", title: "License" },
   { id: "main-skill", title: "Skill" },
-  { id: "main-project", title: "Project" },
+  { id: "main-career", title: "Career" },
   // { id: "main-testimonial", title: "Testimonial" },
   // { id: "main-article", title: "Article" },
 ];
@@ -46,8 +46,8 @@ export default function Header() {
   }, []);
 
   return (
-    <header className="sticky top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
-      <nav className="mt-4 relative max-w-2xl w-full bg-white border border-gray-200 rounded-[2rem] mx-2 py-2.5 md:flex md:items-center md:justify-between md:py-0 md:px-4 md:mx-auto dark:bg-neutral-900 dark:border-neutral-700">
+    <header className="sticky top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm ">
+      <nav className="mt-1 relative max-w-2xl w-full bg-gray-50 border border-gray-200 rounded-[2rem] mx-2 py-2.5 md:flex md:items-center md:justify-between md:py-0 md:px-4 md:mx-auto dark:bg-neutral-800 dark:border-neutral-00">
         <div className="px-4 md:px-0 flex justify-between items-center">
           <div>
             <a
