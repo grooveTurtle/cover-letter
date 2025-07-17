131dd28e5291e89e3e3238236bb60a35fdd921be
grooveTurtle <grooveturtle29@gmail.com>
2025-07-17 23:32:13 +0900

    배포 준비

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const repoName = "cover-letter";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: `/${repoName}`,
+  basePath: `/${repoName}`,
+  trailingSlash: true,
+  output: "export",
 };
 
 export default nextConfig;
